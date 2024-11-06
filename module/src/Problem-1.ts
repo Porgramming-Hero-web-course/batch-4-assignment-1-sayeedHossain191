286b860c18aecba20531230ace39d051f5742c45
sayeedHossain191 <sayeed.hossain.sagor@gmail.com>
2024-11-07 00:07:41 +0600

    problem-1 completed

--- module/src/Problem-1.ts
+++ module/src/Problem-1.ts
@@ -1,1 +1,13 @@
-// Problem 1:
+// Problem 1: Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
+
+const sumArray = (arr: number[]): number => {
+    let sum = 0
+    arr.forEach(item => {
+        sum = sum + item
+    })
+    return sum
+}
+const arrayResult = sumArray([1, 2, 3, 4, 5]);
+
+console.log(arrayResult)
+
