57b1e78f9879d709652a7964f94dcf531c44ad10
sayeedHossain191 <sayeed.hossain.sagor@gmail.com>
2024-11-07 17:34:30 +0600

    problem-6 is running

---
+++ module/src/Problem-6.ts
@@ -0,0 +1,11 @@
+{
+    // Define an interface Profile with properties name, age, and email. Create a function updateProfile that accepts an object of type Profile and an object of type Partial representing the updates. The function should return the updated profile.
+
+    const updateProfile = () => {
+
+    }
+
+    // Sample Input :
+    const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
+    //console.log(updateProfile(myProfile, { age: 26 }));
+}
