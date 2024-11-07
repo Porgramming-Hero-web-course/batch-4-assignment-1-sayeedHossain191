08ae9d815000ae9f9d6bfa0d934ca14974e1a590
sayeedHossain191 <sayeed.hossain.sagor@gmail.com>
2024-11-07 20:46:01 +0600

    problem-6 completed

--- module/src/Problem-6.ts
+++ module/src/Problem-6.ts
@@ -1,11 +1,21 @@
 {
     // Define an interface Profile with properties name, age, and email. Create a function updateProfile that accepts an object of type Profile and an object of type Partial representing the updates. The function should return the updated profile.
 
-    const updateProfile = () => {
+    interface Profile {
+        name: string
+        age: number
+        email: string
+    }
+    type partialProfile = Partial<Profile>
 
+    const updateProfile = (obj1: Profile, obj2: partialProfile): Profile => {
+        if (obj2.age !== undefined) {
+            obj1.age = obj2.age;
+        }
+        return obj1
     }
 
     // Sample Input :
     const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
-    //console.log(updateProfile(myProfile, { age: 26 }));
+    console.log(updateProfile(myProfile, { age: 26 }));
 }
